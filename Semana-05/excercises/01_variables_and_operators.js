3f4def9005b9871be77e1b40feeb9162f0c93232
Juan Moreira <juanmoreira1518@gmail.com>
2022-04-17 15:14:50 -0300

    BaSP-2022:ex03-arrays

--- Semana-05/excercises/01_variables_and_operators.js
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -1,22 +1,22 @@
 /* Crear dos variables numéricas y utilizar el operador suma para guardar
 el valor de la suma de ambos números en una 3er variable. */
 
-let num1 = 17;
-let num2 = 25;
-let suma = num1 + num2;
+var num1 = 17;
+var num2 = 25;
+var suma = num1 + num2;
 console.log(suma);
 
 // Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
-let nombre = 'Juan ';
-let apellido = 'Moreira';
-let nCompleto = nombre + apellido;
+var nombre = 'Juan ';
+var apellido = 'Moreira';
+var nCompleto = nombre + apellido;
 console.log(nCompleto);
 
 /* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
 del string) guardando el resultado de la suma en una 3er variable (utilizar length). */
 
-let string1 = 'este texto tiene 26 letras';
-let string2 = 'este tiene 20 letras';
-let sumaString = string1.length + string2.length;
+var string1 = 'este texto tiene 26 letras';
+var string2 = 'este tiene 20 letras';
+var sumaString = string1.length + string2.length;
 console.log(sumaString);
--- Semana-05/excercises/02_strings.js
+++ Semana-05/excercises/02_strings.js
@@ -1,35 +1,35 @@
 /* Crear una variable de tipo string con al menos 10 caracteres y 
 convertir todo el texto en mayúscula (utilizar toUpperCase). */
 
-let stringAMayus = 'Este string será mostrado en mayúsculas';
+var stringAMayus = 'Este string será mostrado en mayúsculas';
 console.log (stringAMayus.toUpperCase());
 
 /* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string 
 con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
-let stringEx2b = 'Este es un string de cualquiera';
-let stringPrim = stringEx2b.substring(0,3);
+var stringEx2b = 'Este es un string de cualquiera';
+var stringPrim = stringEx2b.substring(0,3);
 console.log (stringPrim);
 /* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string 
 con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
-let stringEx2c = 'Este string también es de prueba';
-let stringUlt = stringEx2c.substring(stringEx2c.length-3, stringEx2c.length);
+var stringEx2c = 'Este string también es de prueba';
+var stringUlt = stringEx2c.substring(stringEx2c.length-3, stringEx2c.length);
 console.log (stringUlt);
 
 /* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la 
 primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable 
 (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
 
-let stringEx2d = 'este sTRING tiene VARias LeTRas en MAyúsCula';
-let solEx2d = stringEx2d.substring(0,1).toUpperCase() + stringEx2d.substring(1,stringEx2d.length).toLowerCase();
+var stringEx2d = 'este sTRING tiene VARias LeTRas en MAyúsCula';
+var solEx2d = stringEx2d.substring(0,1).toUpperCase() + stringEx2d.substring(1,stringEx2d.length).toLowerCase();
 console.log(solEx2d);
 
 /*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar
  la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf). */
 
- let stringEx2e = 'Tengo un espacio en blanco en la sexta posición';
- let posEsp = stringEx2e.indexOf(' ');
+ var stringEx2e = 'Tengo un espacio en blanco en la sexta posición';
+ var posEsp = stringEx2e.indexOf(' ');
  console.log(posEsp + 1);
  
 /*  Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio 
@@ -37,9 +37,9 @@ console.log(solEx2d);
   tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula 
   (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +). */
 
-  let stringEx2f = 'ornitorrincos paralelepípedo';
-  let posEsp2 = stringEx2f.indexOf(' ');
-  let solEx2f = stringEx2f.substring(0, 1).toUpperCase() + stringEx2f.substring(1, posEsp2).toLowerCase()
+  var stringEx2f = 'ornitorrincos paralelepípedo';
+  var posEsp2 = stringEx2f.indexOf(' ');
+  var solEx2f = stringEx2f.substring(0, 1).toUpperCase() + stringEx2f.substring(1, posEsp2).toLowerCase()
   + ' ' + stringEx2f.substring(posEsp2 + 1, posEsp2 + 2).toUpperCase() + 
   stringEx2f.substring(posEsp2 + 2, stringEx2f.length).toLowerCase();
   console.log(solEx2f);
